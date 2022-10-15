1e1e6d19395e2b0246cc8125ba61f73c150167aa
filip-przygoda <filipprzygoda52@gmail.com>
2022-10-15 21:35:26 +0200

    base

--- src/neighborhood/contact/views.py
+++ src/neighborhood/contact/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class ContactView(View):
+    initial = {}
+    template_name = 'contact/contact.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        return render(request=request, template_name=self.template_name)
--- src/neighborhood/neighborhood/settings.py
+++ src/neighborhood/neighborhood/settings.py
@@ -57,7 +57,8 @@ ROOT_URLCONF = 'neighborhood.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(os.path.join(BASE_DIR, 'templates')), ],        'APP_DIRS': True,
+        'DIRS': [],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
